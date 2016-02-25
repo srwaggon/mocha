9a8d12adc002828f3d7184987a45053fd915392b
Samuel Waggoner <samuel.waggoner@gmail.com>
2016-02-25 11:47:56 -0800

    Creates separate runnable applications for server and client. Both can be launched simultaneously. closes #1

--- src/main/java/mocha/server/main/Main.java
+++ src/main/java/mocha/server/main/Main.java
@@ -1,4 +1,12 @@
 package mocha.server.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    primaryStage.setTitle("Mocha Server");
+    primaryStage.show();
+  }
 }
