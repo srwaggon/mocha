eb194fb094aa8be71d16e553d43e93d3a1fe2317
Samuel Waggoner <samuel.waggoner@gmail.com>
2017-10-26 21:22:02 -0500

    Adds sound, sounds

---
+++ src/main/java/mocha/sfx/Sound.java
@@ -0,0 +1,22 @@
+package mocha.sfx;
+
+import java.net.URL;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class Sound {
+
+  private MediaPlayer mediaPlayer;
+
+  Sound(String fileName) {
+    URL resource = getClass().getClassLoader().getResource("sfx/" + fileName);
+    Media media = new Media(resource.toString());
+    this.mediaPlayer = new MediaPlayer(media);
+  }
+
+  public void play() {
+    mediaPlayer.setOnEndOfMedia(mediaPlayer::stop);
+    mediaPlayer.play();
+  }
+}
