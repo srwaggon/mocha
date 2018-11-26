5dd2d3db4c4e4eb47f0f76632efb8da9ab85e4c6
Samuel Waggoner <samuel.waggoner@gmail.com>
2018-11-25 21:16:37 -0800

    Update TileSpriteSelector to utilise newer functions

--- mocha-client/src/main/java/mocha/client/gfx/TileSpriteSelector.java
+++ mocha-client/src/main/java/mocha/client/gfx/TileSpriteSelector.java
@@ -2,13 +2,23 @@ package mocha.client.gfx;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.Optional;
 
 import javax.inject.Inject;
 
+import mocha.game.world.Direction;
 import mocha.game.world.Location;
 import mocha.game.world.chunk.ChunkService;
 import mocha.game.world.tile.TileType;
+
+import static mocha.game.world.Direction.EAST;
+import static mocha.game.world.Direction.NORTH;
+import static mocha.game.world.Direction.SOUTH;
+import static mocha.game.world.Direction.WEST;
+import static mocha.game.world.tile.TileType.GRASS;
+import static mocha.game.world.tile.TileType.STONE;
+import static mocha.game.world.tile.TileType.WATER;
 
 @Component
 public class TileSpriteSelector {
@@ -28,20 +38,16 @@ public class TileSpriteSelector {
   }
 
   private boolean isConnectedTileSprite(TileType type) {
-    return type.equals(TileType.GRASS)
-        || type.equals(TileType.WATER)
-        || type.equals(TileType.STONE);
+    return type.equals(GRASS)
+        || type.equals(WATER)
+        || type.equals(STONE);
   }
 
-  private int getSpriteOffset(Location location, TileType type) {
-    return (isType(location.north(), type) ? 1 : 0) +
-        (isType(location.east(), type) ? 2 : 0) +
-        (isType(location.south(), type) ? 4 : 0) +
-        (isType(location.west(), type) ? 8 : 0);
-  }
-
-  private boolean isType(Location location, TileType type) {
-    Optional<TileType> tileAt = chunkService.getTileAt(location);
-    return tileAt.isPresent() && tileAt.get().equals(type);
+  private int getSpriteOffset(Location tileLocation, TileType type) {
+    Map<Direction, TileType> tileNeighbors = chunkService.getTileNeighbors(tileLocation);
+    return (tileNeighbors.get(NORTH).equals(type) ? 1 : 0) +
+        (tileNeighbors.get(EAST).equals(type) ? 2 : 0) +
+        (tileNeighbors.get(SOUTH).equals(type) ? 4 : 0) +
+        (tileNeighbors.get(WEST).equals(type) ? 8 : 0);
   }
 }
