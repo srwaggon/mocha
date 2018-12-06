fc9594db49553a432d284119c6c0d331bd12bc35
Samuel Waggoner <samuel.waggoner@gmail.com>
2018-12-05 22:31:00 -0800

    Enable entities to move in both dimensions at the same time (x and y), and don't stop them when they run into a wall.

--- mocha-game/src/main/java/mocha/game/world/entity/movement/SlidingMovement.java
+++ mocha-game/src/main/java/mocha/game/world/entity/movement/SlidingMovement.java
@@ -7,12 +7,11 @@ import mocha.game.world.Location;
 import mocha.game.world.entity.Entity;
 import mocha.game.world.entity.movement.collision.Collider;
 import mocha.game.world.entity.movement.collision.Collision;
-import mocha.game.world.tile.TileType;
 import mocha.shared.Repository;
 
 public class SlidingMovement extends BaseMovement {
 
-  private static final int WALK_SPEED = 1;
+  private static final int WALK_SPEED = 2;
 
   private Repository<Entity, Integer> entityRepository;
 
@@ -23,38 +22,26 @@ public class SlidingMovement extends BaseMovement {
 
   @Override
   public void up() {
-    if (isMoving()) {
-      return;
-    }
     this.direction = Direction.NORTH;
-    this.yOffset = direction.getYMultiplier() * TileType.SIZE;
+    this.yOffset = direction.getYMultiplier();
   }
 
   @Override
   public void down() {
-    if (isMoving()) {
-      return;
-    }
     this.direction = Direction.SOUTH;
-    this.yOffset = direction.getYMultiplier() * TileType.SIZE;
+    this.yOffset = direction.getYMultiplier();
   }
 
   @Override
   public void left() {
-    if (isMoving()) {
-      return;
-    }
     this.direction = Direction.WEST;
-    this.xOffset = direction.getXMultiplier() * TileType.SIZE;
+    this.xOffset = direction.getXMultiplier();
   }
 
   @Override
   public void right() {
-    if (isMoving()) {
-      return;
-    }
     this.direction = Direction.EAST;
-    this.xOffset = direction.getXMultiplier() * TileType.SIZE;
+    this.xOffset = direction.getXMultiplier();
   }
 
   @Override
@@ -65,9 +52,8 @@ public class SlidingMovement extends BaseMovement {
   }
 
   private void move() {
-    int xDelta = WALK_SPEED * direction.getXMultiplier();
-
-    int yDelta = WALK_SPEED * direction.getYMultiplier();
+    int xDelta = xOffset * WALK_SPEED;
+    int yDelta = yOffset * WALK_SPEED;
 
     move2(xDelta, 0);
     move2(0, yDelta);
@@ -81,13 +67,6 @@ public class SlidingMovement extends BaseMovement {
     informColliders(colliders);
 
     if (colliders.stream().anyMatch(Collider::isBlocking)) {
-      if (xDelta != 0) {
-        this.xOffset = 0;
-      }
-
-      if (yDelta != 0) {
-        this.yOffset = 0;
-      }
       return;
     }
 
